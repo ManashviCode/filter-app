dff66ba6b5c0935925cbe41b297f06828fb5cd66
Divani Perera <divamanashvi03@gmail.com>
2024-05-26 09:46:28 +0530

    added price change in filterPanel and images

--- src/components/Home/FilterPanel/index.jsx
+++ src/components/Home/FilterPanel/index.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { categoryList, ratingList } from '../../../constants'
 import FilterListToggle from '../../common/FilterListToggle'
 import './styles.css'
+import CheckboxProton from '../../common/CheckboxProton';
+import SliderProton from '../../common/SliderProton';
 
 const FilterPanel=({
   selectedCategory, 
   selectToggle,
   selectedRating,
-  selectRating
+  selectRating,
+  cuisines,
+  changeChecked,
+  selectedPrice,
+  changedPrice
+
 })=> {
   return (
     <div>
@@ -24,9 +31,19 @@ const FilterPanel=({
         {/*Cusines*/}
         <div className="input-group">
           <p className='label'> Cuisine</p>
+          {cuisines.map(cuisine=>
+          <CheckboxProton
+          key={cuisine.id}
+          cuisine={cuisine}
+          changeChecked={changeChecked}
+          />)}
         </div>
 
         {/*Price Range */}
+        <div className='input-group'>
+      <p className='label-range'>Price Range</p>
+      <SliderProton value={selectedPrice} changedPrice={changedPrice} />
+        </div>
 
         {/*Star Rating */}
         <div className='input-group'>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,28 +2,41 @@ import React, { useState } from 'react'
 import SearchBar from '../../components/Home/SearchBar';
 import FilterPanel from '../../components/Home/FilterPanel';
 import List from '../../components/Home/List';
+import EmptyView from '../../components/common/EmptyView';
+import { dataList } from '../../constants';
 import './style.css'
 
 const Home =() => {
     const [selectedCategory, setSelectedCategory]=useState(null);
     const [selectedRating, setSelectedRating]=useState(null);
-    const [cusines, setCusines]=useState([
-{
-    id: 1, checked:false, label:'American',
-},
-{
-    id: 2, checked:false, label:'Chinese',
-},
-{
-    id: 3, checked:false, label:'Italian',
-},
-]);
+    const [selectedPrice, setSelectedPrice]=useState([1000,5000]);
+    
+    const [cuisines, setCuisines]=useState([
+        {id: 1, checked:false, label:'American'},
+        {id: 2, checked:false, label:'Chinese'},
+        {id: 3, checked:false, label:'Italian'},
+    ]);
+
+    const [resultsFound, setResultsFound] = useState(true);
+    const [list, setList] = useState(dataList);
 
     const handleSelectCategory=(event,value)=>
         !value ? null :setSelectedCategory(value);
 
     const handleSelectRating=(event,value)=>
         !value ? null :setSelectedRating(value);
+
+    const handleChangeChecked = (id) => {
+        const cusinesStateList = cuisines;
+        const changeCheckedCuisines = cusinesStateList.map((item) =>
+          item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        setCuisines(changeCheckedCuisines);
+      };
+
+    const handleChangePrice = (event, value) => {
+        setSelectedPrice(value);
+      };
 
     return (
     <div className='home'>
@@ -39,12 +52,16 @@ const Home =() => {
                     selectedCategory={selectedCategory}
                     selectRating={handleSelectRating}
                     selectedRating={selectedRating}
+                    cuisines={cuisines}
+                    changeChecked={handleChangeChecked}
+                    selectedPrice={selectedPrice}
+                    changedPrice={handleChangePrice}
                 />
             </div>
 
+            {/* List & Empty View */}
             <div className='home_list-wrap'>
-                {/* List & Empty View */}
-                <List/>
+            {resultsFound ? <List list={list} />: <EmptyView />}
             </div>
         </div>
     </div>
